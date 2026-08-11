411d7341336c79d38d7f3fa31aad9d19907536d8
DF11ZomGraves <zom1995@126.com>
2026-08-12 00:50:47 +0800

    Create branch 1.21.1

--- src/main/java/df11zomgraves/ingameinfo/parser/text/InvalidTokenException.java
+++
@@ -1,7 +0,0 @@
-package df11zomgraves.ingameinfo.parser.text;
-
-public class InvalidTokenException extends Exception {
-    public InvalidTokenException(final String string) {
-        super(string);
-    }
-}
